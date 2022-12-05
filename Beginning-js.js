5a46446ed65046925549a441bea72e00d21b8812
anwilk <awilk@lc.edu>
2022-12-05 16:24:53 -0600

    EOD commit 2022-12-05

--- Beginning-js.js
+++ Beginning-js.js
@@ -1,1 +1,20 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7"
+var width = 900;
+      var height = 600;
+
+      var projection = d3.geo.mercator();
+      
+      var svg = d3.select("body").append("svg")
+          .attr("width", width)
+          .attr("height", height);
+      var path = d3.geo.path()
+          .projection(projection);
+      var g = svg.append("g");
+      
+      d3.json("world-110m2.json", function(error, topology) {
+          g.selectAll("path")
+            .data(topojson.object(topology, topology.objects.countries)
+                .geometries)
+          .enter()
+            .append("path")
+            .attr("d", path)
+      });
