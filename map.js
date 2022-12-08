d182eb7673ece4c76eaac9b81ef59036912e135b
anwilk <andrewwilk2@gmail.com>
2022-12-07 20:15:46 -0600

    Hotfix 0.0.1

--- map.js
+++ map.js
@@ -1,9 +1,8 @@
 //Create an asynchronous function which allows us to use keword 'await'
 async function load_and_plot() {
-  
   //Pause until we read state json
-  const states = await d3.json("./features_simplified/states_reduced.json"); 
-  
+  const states = await d3.json("./features_simplified/states_reduced.json");
+
   //Pause until we get institute geojson
   const points = await d3.json("./features_simplified/institute_poi.geojson");
 
@@ -14,35 +13,33 @@ async function load_and_plot() {
   //Variables for svg container
   var width = 900,
     height = 600;
-  
+
   //Create the container itself
-  var container = d3.select('#content')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-  
+  var container = d3
+    .select("#content")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
   //
-  
+
   //id for basemap that belongs to map class
-  var basemap = container
-    .append('g')
-    .attr('class', 'map')
-    .attr("id", "base")
-  
+  var basemap = container.append("g").attr("class", "map").attr("id", "base");
+
   //ID for points that belongs to map class
   var point_overlay = container
-    .append('g')
-    .attr('id', 'points')
-    .attr('class', 'point_data')
-  
+    .append("g")
+    .attr("id", "points")
+    .attr("class", "point_data");
+
   // Join the FeatureCollection's features array to path elements
   d3.select("#base") //Identify what html element to plot into
     .selectAll("path") //select (or create) path element for svg block
     .data(states.features) //use the features of the states
     .join("path") //join states data to the path
-    .attr("d", geoGenerator) //Use geo generator to assign value to "d" attribute 
-    .attr("fill", "none")
-    
+    .attr("d", geoGenerator) //Use geo generator to assign value to "d" attribute
+    .attr("fill", "none");
+
   //Separate element used so that mouseover interaction is only applied to points
   d3.select("#points")
     .selectAll("path")
@@ -52,82 +49,80 @@ async function load_and_plot() {
     .attr("name", "test")
     .attr("fill", "darkgrey")
     .attr("stroke", "black")
-    .on('mouseenter', showTooltip)
-    .on('mouseout', hideTooltip)
-    .on('click', showDetails)
-  
-// Tooltip on mouseover section ----------
-// We add a <div> container for the tooltip, which is hidden by default.
-var tooltip = d3.select("#content")
-  .append("div")
-  .attr("class", "tooltip hidden");
+    .on("mouseenter", showTooltip)
+    .on("mouseout", hideTooltip)
+    .on("click", showDetails);
 
-//Function to hide tooltip on mouse out
-function hideTooltip() {
-  tooltip.classed('hidden', true);
-}
-  
+  // Tooltip on mouseover section ----------
+  // We add a <div> container for the tooltip, which is hidden by default.
+  var tooltip = d3
+    .select("#content")
+    .append("div")
+    .attr("class", "tooltip hidden");
+
+  //Function to hide tooltip on mouse out
+  function hideTooltip() {
+    tooltip.classed("hidden", true);
+  }
+
   //Create a function to display data in tooltip
-function showTooltip(event, datum) {
-  // Get the ID of the feature.
-  var id = datum.properties;
-  
-  // Get location of mouse
-   // Get the current mouse position (as integer)
-  var mouse_pos = d3.pointer(event, this).map(d => parseInt(d))
-  
-  // Calculate the absolute left and top offsets of the tooltip. If the
-  // mouse is close to the right border of the map, show the tooltip on
-  // the left.
-  var left = Math.min(width - 4 * id.location.length, mouse_pos[0] + 30);
-  var top = mouse_pos[1] + 30;
+  function showTooltip(event, datum) {
+    // Get the ID of the feature.
+    var id = datum.properties;
 
-  // Use the ID to get the string in 'location'
-  tooltip.classed('hidden', false)
-    .attr("style", "left:" + left + "px; top:" + top + "px")
-    .html(id.location);
-  
-  console.log(id.location)
-  console.log(id.location.length)
-  console.log(mouse_pos)
-}
+    // Get location of mouse
+    // Get the current mouse position (as integer)
+    var mouse_pos = d3.pointer(event, this).map((d) => parseInt(d));
 
-//Details on click section --------
+    // Calculate the absolute left and top offsets of the tooltip. If the
+    // mouse is close to the right border of the map, show the tooltip on
+    // the left.
+    var left = Math.min(width - 4 * id.location.length, mouse_pos[0] + 30);
+    var top = mouse_pos[1] + 30;
+
+    // Use the ID to get the string in 'location'
+    tooltip
+      .classed("hidden", false)
+      .attr("style", "left:" + left + "px; top:" + top + "px")
+      .html(id.location);
+
+    console.log(id.location);
+    console.log(id.location.length);
+    console.log(mouse_pos);
+  }
+
+  //Details on click section --------
   async function showDetails(event, datum) {
-    
-   d3.select('table').remove()
+    d3.select("table").remove();
 
     //Get data
-    const obj = (datum.properties)
-    const entries = Object.entries(obj)
+    const obj = datum.properties;
+    const entries = Object.entries(obj);
 
     //Create table in the detials <div>
-    var table = d3.select('#details').append('table')
-    thead = table.append("thead").append("tr")
-    tbody = table.append('tbody')
+    var table = d3.select("#details").append("table");
+    thead = table.append("thead").append("tr");
+    tbody = table.append("tbody");
 
     //Create row for each "data"
-    var rows = tbody
-        .selectAll('tr')
-        .data(entries)
-        .enter()
-      .append('tr')
-    
+    var rows = tbody.selectAll("tr").data(entries).enter().append("tr");
+
     //Create table cells
-    var td = rows.selectAll("td")
-      .data(function (d) { return d; })
+    var td = rows
+      .selectAll("td")
+      .data(function (d) {
+        return d;
+      })
       .enter()
       .append("td")
       .append("a")
       //.html(function (d) { if (JSON.stringify(d).includes("http")) {return `"<a href = ${d}</a>"`} else { return d } })
-      .html(function(d){return(d)})
+      .text(function (d) {
+        return d;
+      });
 
-
-    console.log(entries)
-
-
+    console.log(entries);
   }
-    
 }
 
 load_and_plot();
